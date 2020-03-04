d35de08387b7e61947d7de2efe4e3183bea59f9d
Roy van Bragt <roy@van-bragt.com>
2020-03-04 18:25:42 +0100

    Refined hash, added token generation.

--- Core/toolbox.py
+++ Core/toolbox.py
@@ -1,6 +1,11 @@
-import hashlib
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
-def server_hash(data):
+from Core.models import AccountVerification, SCTFUser
+
+import hashlib
+import datetime
+
+def GenerateHash(data):
     """Generates a server-side hash using the SHA-256 algorithm.
     Encode function ensures the input data is a string."""
     
@@ -8,10 +13,29 @@ def server_hash(data):
 
     return hash
 
-def compare_hash(client_hash, server_hash):
+def ValidateHash(client_hash, server_hash):
     """Compares the server-side generated hash to the client-side hash."""
 
-    if client_hash != server_hash:
-        return False
-    else:
-        return True
+    return client_hash != server_hash
+
+def GenerateToken(user):
+    """Generates a token for new users to validate
+    the registration and activate an account."""
+
+    token = PasswordResetTokenGenerator()
+    creation_date = datetime.datetime.today()
+    expiry_date = creation_date + datetime.timedelta(days=1)
+
+    AC = AccountVerification(user=user,
+                     expiry_date=expiry_date,
+                     verification_token=token)
+    
+    # AC.objects.create()
+
+    AC.save()
+
+    return user, token, expiry_date
+
+user = SCTFUser.objects.get(username="roy")
+token = GenerateToken(user=user)
+print(token)
